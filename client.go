3881851535726bd47c1a86a589a889d631967d4a
Roby Daquilante <robydaquilante@gmail.com>
2019-04-12 14:27:21 -0700

    remove main.go

--- client.go
+++ client.go
@@ -9,6 +9,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// Package dragonchain is used to interact programmatically with dragonchains.
 package dragonchain
 
 import (
